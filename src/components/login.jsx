714725056ec12d8db5f7cea1a9c1d69e98531a7a
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-08-06 13:28:37 +0530

    updated login page

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,6 @@
 import './loginPage.css'
+import { MdKeyboardArrowRight } from 'react-icons/md';
+
 
 const Login = () => {
 
@@ -8,16 +10,19 @@ const Login = () => {
       <div className=" d-flex justify-content-center align-items-center pt-5 p-3" >
         <h2><b>HOW DO YOU WANT TO LOGIN?</b></h2>
       </div>
-      <div  className="howToLogin d-flex flex-column justify-content-center align-items-center mt-3">
-      <div className='loginWith'>
-       <h4>login with email</h4>
+      <div  className="howToLogin d-flex flex-column justify-content-center align-items-start  mt-3">
+      <div className='loginWith d-flex align-items-center justify-content-between rounded-4'>
+       <h5 className='loginWith2'>login with email</h5>
+       <p><MdKeyboardArrowRight/></p>
        </div>
-      <hr/>
+      <hr className='horizontal-line'/>
 
-       <div className='d-flex dontHaveAccount'>
-       <h5 >Dont you have an account </h5>
-       <h5 className='signIn mx-2'>Sign in</h5>
+       <div className='dontHaveAccount d-flex align-items-center rounded-4'>
+       <h5 className='dontHaveAccount2'>Dont you have an account <span className='signIn'>Signin</span></h5>
+       {/* <h5 className='signIn'>Sign in</h5> */}
+       <p className='arrow'><MdKeyboardArrowRight/></p>
        </div>
+      
       </div>
     </> 
   )
