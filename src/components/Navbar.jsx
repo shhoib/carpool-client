26737812b79ff91c0f01ef886e732c793d0c1dc8
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-08-12 22:08:30 +0530

    implemented google auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { BiSolidUser } from 'react-icons/bi';
+import { BiSolidUserCircle } from 'react-icons/bi';
+import { AiOutlineMessage,AiOutlineUser } from 'react-icons/ai';
+import { BsFillCarFrontFill } from 'react-icons/bs';
 import './navbar.css'
 
+
 function NavigationBar() {
+
   return (
     <Navbar className="nav" >
       <Container>
         <Navbar.Brand className='coRide-text'>CoRide</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <NavDropdown className='text-white ' title={<BiSolidUser size={29} />} id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1"><h6 className='h-six-tags'> Profile</h6></NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2"><h6 className='h-six-tags'>Inbox</h6></NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3"><h6 className='h-six-tags'>My Rides</h6></NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3"><h6 className='h-six-tags'>Payment and refunds</h6></NavDropdown.Item>
+          <NavDropdown className='text-white ' title={<BiSolidUserCircle size={29} />} id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1"><h5 className='h-six-tags'><AiOutlineUser/> Profile</h5></NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2"><h5 className='h-six-tags'><AiOutlineMessage/> Inbox</h5></NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3"><h5 className='h-six-tags'><BsFillCarFrontFill/> My Rides</h5></NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4"><h5>Logout</h5> </NavDropdown.Item>
           </NavDropdown>
         </Navbar.Collapse>
       </Container>
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,6 +2,8 @@ import { Container } from "react-bootstrap"
 import './profile.css'
 import {BiPlusCircle} from 'react-icons/bi'
 import {AiFillEdit} from 'react-icons/ai'
+import {MdKeyboardArrowRight} from 'react-icons/md'
+import {GiReceiveMoney} from 'react-icons/gi'
 
 
 const Profile = () => {
@@ -16,19 +18,33 @@ const Profile = () => {
 
     <Container className="d-flex mt- justify-content-around align-items-center">
         <h1 className="m-5">SHOIB</h1>
-        <div className="user-profile-image mx-5" ></div>   
+        <div className="user-profile-image" ></div>   
     </Container>
 
     <Container className="profile-and-rating d-flex flex-column my-3">
 
-      <div className=" add-profile-section d-flex align-items-center my-">
+      <div className="add-profile-section p-1 d-flex justify-content-between">
+       <div className="  d-flex align-items-center my-">
         <h2 className="mx-3"><BiPlusCircle/></h2>
-        <h4>Add profile picture</h4>
+        <h5>Add profile picture</h5>
+       </div>
+        <h4><MdKeyboardArrowRight/></h4>
       </div>
 
-      <div className="add-profile-section d-flex align-items-center">
+      <div className="add-profile-section  p-1 d-flex justify-content-between">
+        <div className="d-flex align-items-center">
         <h2 className="mx-3"><AiFillEdit/></h2>
-        <h4>Edit personal details</h4>
+        <h5>Edit personal details</h5>
+        </div>
+        <h4><MdKeyboardArrowRight/></h4>
+      </div>
+
+      <div className="add-profile-section p-1 d-flex justify-content-between">
+       <div className="  d-flex align-items-center my-">
+        <h2 className="mx-3"><GiReceiveMoney/></h2>
+        <h5>Payment and refund</h5>
+       </div>
+        <h4><MdKeyboardArrowRight/></h4>
       </div>
     </Container>
 
@@ -37,12 +53,18 @@ const Profile = () => {
      </div>
 
     <Container className="profile-and-rating">
-      <div className="rating-and-changePassword">
-        <h4>Ratings</h4>
+      <div className="rating-and-changePassword d-flex justify-content-between">
+       <div>
+        <h5>Ratings</h5>
+       </div>
+       <h4><MdKeyboardArrowRight/></h4>
       </div>
 
-      <div className="rating-and-changePassword">
-        <h4>Change Password</h4>
+      <div className="rating-and-changePassword d-flex justify-content-between">
+        <div>
+         <h5>Change Password</h5>
+        </div>
+        <h4><MdKeyboardArrowRight/></h4>
       </div>
     </Container>
     
