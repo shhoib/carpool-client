3b48dd238e61d5fa68851e3c14431048b7983e13
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-09-10 15:19:17 +0530

    my rides

--- src/components/myRides.jsx
+++ src/components/myRides.jsx
@@ -2,20 +2,26 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux"
 import axiosInstance from "../api/axios";
 import { useState } from "react";
-import Tabs from '@mui/material/Tabs';
+// import Tabs from '@mui/material/Tabs';
+import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import './myRides.css'
 
 
 const MyRides = () => {
 
     const USER = useSelector((state)=>state.userAuth);
-      const [value, setValue] = useState(2);
 
+      const [value, setValue] = useState(1);
+      const [myRides,setMyRides] = useState([])
 
-    const [myRides,setMyRides] = useState([])
+      useEffect(() => {
+      setValue("1");
+      }, []);
+
 
     useEffect(() => {
     const rides = async () => {
@@ -38,19 +44,18 @@ const MyRides = () => {
 
   return (
     <>
-    <h1>hii</h1>
     <Box sx={{ width: '100%', typography: 'body1' }}>
       <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
-            <Tab label="Item One" value="1" />
-            <Tab label="Item Two" value="2" />
-            <Tab label="Item Three" value="3" />
+        <Box className="tablist-container d-flex align-items-center justify-content-center" sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <TabList className='tablist' onChange={handleChange} aria-label="lab API tabs example">
+            <Tab label="Hosted Rides" value="1" />
+            <Tab  label="current Rides" value="2" />
+            <Tab label="Joined Rides" value="3" />
           </TabList>
         </Box>
-        <TabPanel value="2">Item Oe</TabPanel>
+        <TabPanel value="1"><h1>hq</h1></TabPanel>
         <TabPanel value="2">Item Two</TabPanel>
-        <TabPanel value="1">Item Three</TabPanel>
+        <TabPanel value="3">Item three</TabPanel>
       </TabContext>
     </Box>
     </>
