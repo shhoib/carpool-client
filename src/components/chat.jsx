fd350d8a963b7efb21a1c7fa6bdd45b0c85323a8
Shuaib Salam <shuaibsalam1234@gmail.com>
2023-09-12 08:17:25 +0530

    chat ui creating

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,8 +1,20 @@
 // import React from 'react'
+import {Container} from 'react-bootstrap'
+import './chat.css'
 
 const Chat = () => {
+
+
   return (
-    <div>Chat</div>
+    <>
+  <Container className='chat-container'>
+  <div className=''>
+  <input className='message-input'/>
+  </div>
+  <button>Send</button>
+    
+  </Container>
+    </>
   )
 }
 
